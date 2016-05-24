2bdaa681a7be7c08ec0109a0bec414925de862bf
Mike Sciking <sciking@users.noreply.github.com>
2016-05-24 19:28:29 +0200

    Update renziesim.py

--- renziesim.py
+++ renziesim.py
@@ -40,16 +40,30 @@ Benvenuto nel simulatore di Matteo Renzi"""
 def giuoco():
 	clear()
 	global elettori
-	jlm = random.randint(-5,5)
-	if jlm > 0:
-		print "Tanti nuovi elettori ci hanno scelto"
-	else:
-		print "I cattivoni della LN ci hanno tolto voti"
+	jlm = random.randint(1,6)
+	if jlm == 1:
+		print "Figura di merda di un tuo ministro coi Partigiani!"
+		elettori = elettori + random.randint(-5,0)
+	elif jlm == 2:
+		print "PD vince le elezioni europee"
+		elettori = elettori + random.randint(1,5)
+	elif jlm == 3:
+		print "M5S e LN hanno sempre più voti"
+		elettori = elettori - random.randint(1,5)
+	elif jlm == 4:
+		print "Il Popolo si astiene al Referendum a te inviso"
+		elettori = elettori + 3
+	elif jlm == 5:
+		print "Non ti danno la flessibilità a Bruxelles: Italia rischia di fallire"
+		elettori = elettori - 5
+	elif jlm == 6:
+		print "Parli al Parlamento Europeo"
+		elettori = elettori + random.randint(1,4)
 	print "*"*45
 	print "Elettori", elettori
 	print """Che vuoi fare?
 1) Inciucio
-2) Presenta candidato inpresentabile in qualche città italiana
+2) Presenta candidato impresentabile in qualche città italiana
 3) Vai contro alle richieste autonomiste in Lombardia e in Veneto
 4) Modifica la Costituzione
 5) Difendi ministro che ha fatto gaffe
@@ -57,6 +71,8 @@ def giuoco():
 7) Invita all'astensione
 8) Ice Bucket Challenge
 9) Talk su Facebook
+10) Lezione di civiltà all'Austria
+11) Incontra il Papa
 """
 	l = input(">")
 	if l == 1:
@@ -115,7 +131,21 @@ def giuoco():
 		print "Parli con i tuoi fans su facebook"
 		k = random.randint(-4,3) 
 		elettori = elettori + k
-
+	elif l == 10:
+		print "Dai una lezione di civiltà all'Austria"
+		k = random.randint(-6,3)
+		elettori = elettori + k
+		if k > 0:
+			print "Il popolo pensa che tu sia il nuovo Craxi alle prese con Sigonella"
+		else:
+			print "Il popolo ti deride perché non rispetti la Carta Europea delle Autonomie Locali e delle Lingue Regionali o minoritarie"		
+	elif l == 11:
+		print "Visiti il Papa. La chiesa è contenta di cotanta obbedienza"
+		k = random.randint(-4,3) 
+		elettori = elettori + k
+	elif l == 956:
+		elettori = input(">")
+		
 	invio()
 	if elettori < 25:
 		print "Hai pochi elettori: Non puoi più governare il paese"
